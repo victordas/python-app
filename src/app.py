0e46cd11d7bba6e6ad321722131b01899a00b701
davikt <victordas@msn.com>
2025-06-24 13:49:57 +0200

    fix timestamp

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 def details():
     return jsonify({
-        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B d%, %Y"),
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
     }), 200
 
